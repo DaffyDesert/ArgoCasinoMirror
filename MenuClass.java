516f45c91e9186a50d9888d13b44c084c2eedb28
Daniel Miller <dm193@students.uwf.edu>
2023-02-05 23:17:29 -0600

    initial menu pushed

---
+++ MenuClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author daniel miller
+ *
+ */
+public class MenuClass {
+
+	private String menuString;
+	
+	public MenuClass() {
+		setMenuString("menu string is in here");
+	}
+
+	public String getMenuString() {
+		return menuString;
+	}
+
+	public void setMenuString(String menuString) {
+		this.menuString = menuString;
+	}
+}
