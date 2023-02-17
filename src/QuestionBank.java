af3cad49e1803f308960e4dc74413d29efbdb679
Daniel Miller <111814034+djmil12@users.noreply.github.com>
2023-02-16 22:10:25 -0600

    Create QuestionBank.java
    QuestionDifficulty functionaltiy added

---
+++ src/QuestionBank.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+/** dhfjdhj
+ * @Author Daniel Miller
+ * @author dm193
+ *
+ */
+public class QuestionBank {
+	private int dificulty;
+	private String question;
+	
+	ArrayList<String> Qbank;
+	
+	public QuestionBank() {/*this is a default constructor*/}
+	
+	public void addQuestion(String question, String answer, int dificulty) {
+		/*this is code that adds a question*/
+		}
+
+}
