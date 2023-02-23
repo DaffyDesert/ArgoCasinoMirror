e7781895505b8b495176ad31dedeb9167adbf0d4
Alex DeAngelis <ajd61@students.uwf.edu>
2023-02-23 15:23:24 -0600

    Added some stuff to the Game Interface and Player abstract Class. I had an idea for how these would work, though I'm not sure they'll fit with other people's files. Will work out further with group.

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,26 @@
 import javax.swing.JPanel;
 
 public interface Game {
-	public Stopwatch watch; //The stop watch
-	public Thread watchThread; //The thread that allows the watch to run alongside the program.
-	public Board gameBoard; //Danny's board object. Implementation may need work
-	public Player player; //Object of player abstract class. May be subclass
-	void startWatch(); //Starts the stop watch
-	void startGame(); //Starts the game. Calls startWatch()
-	void stopWatch(); //Stops the watch
-	void stopGame(); //Stops the game. Calls stopWatch();
-	JPanel display(); //Returns JPanel to main GUI which displays the game.
-	boolean isOver(); //Checks if the game is over.
+	/*
+	 * Objects to be constant and unchanging across each game
+	 */
+	public Stopwatch watch = new Stopwatch(); //The stop watch
+	public Thread watchThread = new Thread(watch); //The thread that allows the watch to run alongside the program.
+	
+	/*
+	 * Objects that will be different between each game.
+	 */
+	//public Board gameBoard;
+	//public Player player; //May be replaced with game's respective player class, which should extend Player class.
+	//public Player enemy; //Where applicable
+	
+	/*
+	 * Methods that should be implemented in each game. This list may grow as time goes on
+	 */
+	public void startWatch(); //Starts the stop watch
+	public void startGame(); //Starts the game. Calls startWatch()
+	public void stopWatch(); //Stops the watch
+	public void stopGame(); //Stops the game. Calls stopWatch();
+	public JPanel display(); //Returns JPanel to main GUI which displays the game.
+	public boolean isOver(); //Checks if the game is over.
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
 
+//Implementation may vary.
 public abstract class Player {
+	/*
+	 * Object that should be standard across player classes.
+	 */
+	private Deck playerDeck;
+	
+	/*
+	 * Objects that may be different across different subclasses.
+	 */
+	//private Card activeCard; //May be used to represent an active card in War
+	//private ArrayList<Card> hand; //May be used to represent your hand in Blackjack.
+	
+	/*
+	 * Methods
+	 */
+	public Player(Deck startingDeck) {
+		playerDeck = startingDeck;
+	}
 	
 }
