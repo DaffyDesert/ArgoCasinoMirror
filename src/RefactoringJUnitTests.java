8b1387f9c234a4eb4ef86a4534e17a80636a13ba
dwp14 <dwp14@students.uwf.edu>
2023-04-03 12:56:41 -0500

    Update Junit tests

--- src/RefactoringJUnitTests.java
+++ src/RefactoringJUnitTests.java
@@ -68,17 +68,37 @@ public class RefactoringJUnitTests {
 	@Test
 	public void TestShuffleStack() {
 		CardStack cs1 = new CardStack();
+		CardStack cs2 = new CardStack();
 		
 		cs1.addToTop(new Card(1, "S"));
 		cs1.addToTop(new Card(3, "S"));
 		cs1.addToTop(new Card(13, "D"));
 		cs1.addToTop(new Card(11, "H"));
 		
-		cs1.printStack();
+		cs2.addToTop(new Card(1, "S"));
+		cs2.addToTop(new Card(3, "S"));
+		cs2.addToTop(new Card(13, "D"));
+		cs2.addToTop(new Card(11, "H"));
+		
+		assertTrue(isSame(cs1, cs2));
 		
 		cs1.shuffleStack();
 		
-		cs1.printStack();
-
+		assertTrue(!isSame(cs1, cs2));		
+	}
+	
+	public boolean isSame(CardStack cs1, CardStack cs2) {
+		if(cs1.getStackSize() != cs2.getStackSize()) {
+			return false;
+		}
+		
+		for(int i = 0; i < cs1.getStackSize(); ++i) {
+			String card1 = cs1.getStack().get(i).toString();
+			String card2 = cs2.getStack().get(i).toString();
+			if(card1.compareTo(card2) != 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
