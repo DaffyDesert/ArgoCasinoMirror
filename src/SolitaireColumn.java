953ddbb26d1b04af097079edbe77c8bf30219d7e
zach wilson <zdw@students.uwf.edu>
2023-04-27 11:20:16 -0500

    Added 7 solitare columns

--- src/SolitaireColumn.java
+++ src/SolitaireColumn.java
@@ -1,3 +1,8 @@
+import java.awt.Dimension;
+import java.awt.Point;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
 public class SolitaireColumn {
@@ -12,7 +17,12 @@ public class SolitaireColumn {
 	}
 
 	boolean addCard(Card card) {	
-		if(isEmpty()) {
+		if(card.isFaceDown()) {
+			column.getStack().add(card);
+			return true;
+		}
+		
+		else if(isEmpty()) {
 			if(isKing(card)) {
 				column.getStack().add(card);
 				return true;
@@ -22,7 +32,7 @@ public class SolitaireColumn {
 		}
 		else {
 			Card topCard = column.peekCard(column.getStackSize() - 1);
-	        if (isAdjacentAndDifferentColor(topCard, card)) {
+	        if ((isAdjacentAndDifferentColor(topCard, card)) || (topCard.isFaceDown())) {
 	            column.getStack().add(card);
 	            return true;
 	        } else {
@@ -62,7 +72,28 @@ public class SolitaireColumn {
 
 	JPanel display() {
 
-		return null;
+		int numCards = column.getStack().size();
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		panel.setBackground(null);
+		panel.setBorder(null);
+		JLayeredPane lp = new JLayeredPane();
+		lp.setPreferredSize(new Dimension(135,750));
+		Point origin = new Point(10,310);
+		int offset = 50;
+	
+		for(int i = numCards - 1; i >= 0; --i) {
+			JPanel currCard = column.getStack().get(i).draw();
+			
+			currCard.setBounds(origin.x, origin.y,150,150);
+	
+			lp.add(currCard);
+			origin.y -= offset;
+		}
+		lp.setVisible(true);
+		panel.add(lp);
+		panel.setVisible(true);
+		return panel;
 	}
 	
 	void printColumn() {
