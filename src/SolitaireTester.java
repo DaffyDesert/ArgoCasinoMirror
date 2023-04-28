6a1c9c9c194f6afbd070039cdfa6e960c5cbaf73
dwp14 <dwp14@students.uwf.edu>
2023-04-28 11:52:53 -0500

    update columntester

--- src/SolitaireTester.java
+++ src/SolitaireTester.java
@@ -5,11 +5,6 @@ public class SolitaireTester {
 	public static void main(String[] args) {
 		Solitaire sol = new Solitaire();
 
-		
-
-		
-		
-		
 		JFrame frame = new JFrame();
 		frame.setSize(1270,720);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
