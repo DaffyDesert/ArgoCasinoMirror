ac6d0d5bbceb92ca3a780153d710a5da8d2e36c9
RoeHoll <rh114@students.uwf.edu>
2023-04-28 13:36:03 -0500

    Draft

--- src/SolitaireBoard.java
+++ src/SolitaireBoard.java
@@ -2,30 +2,41 @@ import javax.swing.JPanel;
 
 public class SolitaireBoard {
 	static int NUM_COLUMNS = 7;
-	//static int NUM_FOUNDATIONS = 4;
+	static int NUM_FOUNDATIONS = 4;
 	JPanel mainPanel;
 	SolitaireColumn columns[];
 	CardStack deck;
 	FoundationStack[] FDStack;
-	public SolitaireColumn getColumnAt(int index) {
+   // HeartStack[] foundations;
+    //SpadeStack[] foundations;
+   // DiamondsStack[] foundations;
+    // ClubStack[] foundations;
+	
+    public SolitaireColumn getColumnAt(int index) {
 		return columns[index];
 	}
 	public CardStack getDeck() {return deck;}
 
 
-	/*public FoundationStack getFoundations(int index ) {
+	public FoundationStack getFoundations(int index ) {
 
 		return FoundationStack [index];
-	}*/
+	}
 	
 	public SolitaireBoard() {
-		//FDStack = new FoundationStack[NUM_FOUNDATIONS];
+		FDStack = new FoundationStack[NUM_FOUNDATIONS];
 		columns = new SolitaireColumn[NUM_COLUMNS];
 		deck = new CardStack();
 		deck.createStandardDeck();
 		deck.shuffleStack();
 	}
+
 	
+ /* HeartPile = new HeartStack[NUM_FOUNDATIONS];
+for (int i = 0; i < NUM_FOUNDATIONS; i++) {
+    HeartPile[i] = new HeartStack();
+}  */
+
 	public void dealToColumns() {
 		for(int i = 0; i < NUM_COLUMNS; ++i) {
 			columns[i] = new SolitaireColumn();
